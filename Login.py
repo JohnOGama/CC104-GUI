f4ec17d6c375b0379f8399e879cda348069b4fd4
John Ogama <jogama696@gmail.com>
2024-01-18 13:48:04 +0800

    new py

--- Login.py
+++ Login.py
@@ -33,8 +33,8 @@ def FeedUI(  login_frame=None, username=None):
         label = ctk.CTkLabel(master=feed_frame,text=f"Hi {username}, hope you doing well", font=("Roboto", 30))
         label.pack(pady=20, padx=10)
 
-        back_button = ctk.CTkButton(master=feed_frame, text="Logout", command=lambda: LogInUI(root,  register_frame, feed_frame), height=40, width=200, font=("Roboto", 15))
-        back_button.pack(pady=12, padx=10)
+        logout_button = ctk.CTkButton(master=feed_frame, text="Logout", command=lambda: logoutHandler(root, feed_frame, register_frame), height=40, width=200, font=("Roboto", 15))
+        logout_button.pack(pady=12, padx=10)
 
 
 def LogInUI(root, register_frame,feed_frame ):
@@ -75,6 +75,12 @@ def RegisterUI(root, login_frame):
     new_username_entry = ctk.CTkEntry(master=register_frame, placeholder_text="New Username", height=40, width=200, font=("Roboto", 15))
     new_username_entry.pack(pady=12, padx=10)
 
+    # firstname_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Firstname", height=40, width=200, font=("Roboto", 15))
+    # firstname_entry.pack(pady=12, padx=10)
+
+    # lastname_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Lastname", height=40, width=200, font=("Roboto", 15))
+    # lastname_entry.pack(pady=12, padx=10)
+
     new_password_entry = ctk.CTkEntry(master=register_frame, placeholder_text="New Password", show="*", height=40, width=200, font=("Roboto", 15))
     new_password_entry.pack(pady=12, padx=10)
 
@@ -84,15 +90,23 @@ def RegisterUI(root, login_frame):
     back_button = ctk.CTkButton(master=register_frame, text="Back to Login", command=lambda: LogInUI(root, register_frame, None), height=40, width=200, font=("Roboto", 15))
     back_button.pack(pady=12, padx=10)
 
+def logoutHandler(root, register_frame, feed_frame):
+    yes = messagebox.askyesno(message="Do you want to logout?")
+    if yes: 
+        LogInUI(root,  register_frame, feed_frame)
+
 def login(root, username_entry, password_entry):
     entered_username = username_entry.get()
     entered_password = password_entry.get()
 
     if entered_username in users.values() and entered_password in users.values():
         print("Authorized")
+        messagebox.showinfo("Successful", "Log in success")
         FeedUI(  login_frame, username=entered_username)     
     else:
         print("Invalid username or password")
+        messagebox.showerror("Warning", "Wrong username or password")
+        
 
 def registerHandler( root, new_username_entry, new_password_entry):
     new_username = new_username_entry.get()
